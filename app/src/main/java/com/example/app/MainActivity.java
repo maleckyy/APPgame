a78e2bc16ea07c8d1cb0904cd1d03daca3314402
maleckyy <kupferpatryk@gmail.com>
2020-12-24 00:01:30 +0100

    first commit

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.BreakIterator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+int pkt=1;
+    int wynik=0;
+
+    public void klik(View view){
+
+
+        wynik=+pkt;
+
+        Toast.makeText(this,wynik,Toast.LENGTH_LONG).show();
+
+    }
+
+public void textView(View view){
+String.valueOf(wynik);
+    TextView textView = (TextView) findViewById(R.id.textView);
+    textView.setText(String.valueOf(wynik));
+
+}
+
+
+}
