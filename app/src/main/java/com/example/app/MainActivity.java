1d24f83f2dcdd5a6ae855d9c7740027b3f3d08e8
maleckyy <kupferpatryk@gmail.com>
2021-01-03 20:55:45 +0100

    final game

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.app;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -14,40 +19,52 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.net.Inet4Address;
+import java.text.DecimalFormat;
+import java.util.concurrent.Delayed;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    int pkt=1;//ilosc za klikniecie
+    int cenamoneyclick=10;
+    int cenamoneypersec=30;
+
+    double mnoznik=1;//dodatkowa procentowość klikniecia
+    double wynik;
+
+    String blad="Nie masz wystarczająco punktów";
+    String akcept="Kupiłeś ulepszenie";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView textView = (TextView) findViewById(R.id.wyniki);
 
-//        ImageView nazwa=(ImageView) findViewById(R.id.imageView5);
-//        AlphaAnimation rot= new AlphaAnimation(0,100);
-//        rot.setDuration(1000);
-//        nazwa.startAnimation(rot);
-    }
-    int pkt=1;//ilosc za klikniecie
-    int cenamoneyclick=10;
-    private Handler handler = new Handler();
-    int dodatek=0;//ilosc na sekunde
-    double mnoznik=1;//dodatkowa procentowość klikniecia
-    double wynik;
-
-String blad="Nie masz wystarczająco punktów";
-String akcept="Kupiłeś ulepszenie";
-
-
-
-    public void pokazwynik(View view){//ekran pokazujacy pkt
-        TextView textView=(TextView) findViewById(R.id.wyniki);
-
-        String wyn=Double.toString(wynik);
+        String wyn = Double.toString(wynik);
 
         textView.setText(wyn);
 
-    }
+        Button button1 = (Button) findViewById(R.id.button4);
+        Button button2 = (Button) findViewById(R.id.button2);
 
+
+        String cenamoneystart = Integer.toString(cenamoneyclick);
+        String cenamoneystart1 = Integer.toString(cenamoneypersec);
+        button1.setTextSize(12);
+        button2.setTextSize(12);
+        if (wynik < cenamoneyclick) {
+            button1.setText("Dodatkowe punkty kliknięcia za " + cenamoneystart);
+            button2.setText("Dodatkowe punkty kliknięcia za " + cenamoneystart1);
+
+
+        }
+
+
+    }
 
     public void addpointclick(View first) {//glowny przycisk kasa za pojedynczy klik
 
@@ -56,61 +73,70 @@ String akcept="Kupiłeś ulepszenie";
 
         TextView textView=(TextView) findViewById(R.id.wyniki);
 
-        String wyn=Double.toString(wynik);
-
+        DecimalFormat df = new DecimalFormat("###.#");
+        String wyn=df.format(wynik);
         textView.setText(wyn);
         wynik+=(pkt*mnoznik);
-
-
 
     }
     public void moreMoneyClick(View view){//dodatkowe pkt za klikniecie
 
+     Button button1=(Button) findViewById(R.id.button4);
+       String cenamoneystring= Integer.toString(cenamoneyclick*2);
+       button1.setTextSize(12);
+        if(wynik<cenamoneyclick){
+            button1.setText("Dodatkowe punkty kliknięcia za "+cenamoneyclick);
+
+        }else{
+            button1.setText("Dodatkowe punkty kliknięcia za "+ cenamoneystring);
+        }
+
+
         if(wynik>cenamoneyclick){
             wynik-=cenamoneyclick;
-            pkt++;
+
             cenamoneyclick*=2;
-            Toast.makeText(this,akcept,Toast.LENGTH_LONG).show();
+            Toast.makeText(this,akcept,Toast.LENGTH_SHORT).show();
+            pkt++;
         }else{
-            Toast.makeText(this,blad,Toast.LENGTH_LONG).show();
+            Toast.makeText(this,blad,Toast.LENGTH_SHORT).show();
         }
-
         TextView textView=(TextView) findViewById(R.id.wyniki);
-
-        String wyn=Double.toString(wynik);
-
+        DecimalFormat df = new DecimalFormat("###.#");
+        String wyn=df.format(wynik);
         textView.setText(wyn);
 
-
-
     }
-    public void moneyPerSec(View view){//pkt na sekunde
-        int cenamoneypersec=20;
+    public void moneyMnoznik(View view){//pkt na sekunde
+
+
+        Button button2=(Button) findViewById(R.id.button2);
+
+
+        String cenamoneystring1= Integer.toString(cenamoneypersec*2);
+        button2.setTextSize(12);
+        if(wynik<cenamoneyclick){
+            button2.setText("Dodatkowe punkty kliknięcia za "+cenamoneypersec);
+
+        }else{
+            button2.setText("Dodatkowe punkty kliknięcia za "+ cenamoneystring1);
+        }
         if(wynik>cenamoneypersec){
             wynik-=cenamoneypersec;
+            cenamoneypersec*=2;
+
+            Toast.makeText(this,akcept,Toast.LENGTH_SHORT).show();
+            mnoznik+=0.2;
+
 
         }else{
-
-        }
-
-
-    }
-    public void boost(View view){//boost na 15 sekund, zwieksza 2 razy zarobek z klikniecia
-        int cenaZaturbo=100;
-        if(wynik>cenaZaturbo){
-            wynik-=cenaZaturbo ;
-
-        }else{
-
+            Toast.makeText(this,blad,Toast.LENGTH_SHORT).show();
         }
         TextView textView=(TextView) findViewById(R.id.wyniki);
-
-        String wyn=Double.toString(wynik);
-
+        DecimalFormat df = new DecimalFormat("###.#");
+        String wyn=df.format(wynik);
         textView.setText(wyn);
+
     }
-
-
-
 
 }
